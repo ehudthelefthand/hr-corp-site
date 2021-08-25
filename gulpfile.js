2ba4de4863bf703b3f54ec7b6b7e69eac45120dc
Peerawat Poombua <peerawat.pm@gmail.com>
2021-08-25 09:28:47 +0700

    setup gulp browser-sync sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')(require('node-sass'))
 
+function serve() {
+    browserSync.init({
+        server: './app'
+    })
 
-function defaultTask(cb) {
-    cb()
+    gulp.watch('app/scss/*.scss', runSass)
+    gulp.watch('app/*.html').on('change', browserSync.reload)
 }
 
-exports.default = defaultTask
+function runSass() {
+    return gulp.src('app/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.stream())
+}
+
+exports.default = gulp.series(runSass, serve)
